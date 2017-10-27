5b192a317817311a8c654cb32cbe543e7117961d
Ole Rößner <oroessner@gmail.com>
2017-10-27 22:17:18 +0200

    do some bad formatting to test the pre-commit hook

--- src/Entity/Activity.php
+++ src/Entity/Activity.php
@@ -39,7 +39,7 @@ class Activity extends AbstractLogEntry implements ArrayableInterface
     public function getData()
     {
         $changed = parent::getData();
-        $data = [];
+        $data = array();
 
         if ($changed) { // is null during deletions
             foreach ($changed as $property => $value) {
@@ -55,10 +55,13 @@ class Activity extends AbstractLogEntry implements ArrayableInterface
         }
 
         return $data;
+
+
+
+
     }
 
-    public function setChangeSet($changeSet)
-    {
+    public function setChangeSet($changeSet) {
         $this->changeSet = $changeSet;
     }
 }
